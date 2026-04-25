3358b66a1c2472b22ea6ec28efd160f55d5a8611
iam-nadiii <naodd43@gmail.com>
2026-04-24 22:37:16 -0500

    created the object class with all its fields each having their own getters and setters. Added a toString method on top.

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,67 @@
+package com.pluralsight;
+
+public class Transaction {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+
+
+
+}
+
+
